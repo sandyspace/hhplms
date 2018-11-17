58cb7ac78cbd2810e4a4ade2382383c9f7dac9fc
shancheng <shanc@133.cn>
2018-11-18 01:45:28 +0800

    更新初始化数据脚本

--- doc/init_data.sql
+++ doc/init_data.sql
@@ -36,6 +36,26 @@ values
 ('addEmployee', '创建雇员', '/ana/employees', NULL, '', NULL, NULL, NULL, NULL, '1', 'api', 'enabled', NULL, 'default', now(), '1'),
 ('editEmployee', '修改雇员', '/ana/employees/:id', NULL, '', NULL, NULL, NULL, NULL, '1', 'api', 'enabled', NULL, 'default', now(), '1');
 
+insert into hhplms.ana_permission
+(name, title, path, redirect_path, component_url, no_cache_flag, hidden_flag, always_show_flag, icon, level, type, status, parent_sid, created_by, created_time, version_num)
+values
+('pendingItemMgmt', '代办事项管理', '/pendingItem', '/pendingItem/list', NULL, 'Y', 'N', 'Y', 'excel', '1', 'page', 'enabled', NULL, 'default', now(), '1'),
+('preferentialMsgMgmt', '优惠信息管理', '/preferentialMsg', '/preferentialMsg/list', NULL, 'Y', 'N', 'Y', 'message', '1', 'page', 'enabled', NULL, 'default', now(), '1'),
+('companyInfoMgmt', '企业管理', '/companyInfo', '/companyInfo/list', NULL, 'Y', 'N', 'Y', 'peoples', '1', 'page', 'enabled', NULL, 'default', now(), '1'),
+('feedbackMgmt', '留言管理', '/feedback', '/feedback/list', NULL, 'Y', 'N', 'Y', 'star', '1', 'page', 'enabled', NULL, 'default', now(), '1');
+
+insert into hhplms.ana_permission
+(name, title, path, redirect_path, component_url, no_cache_flag, hidden_flag, always_show_flag, icon, level, type, status, parent_sid, created_by, created_time, version_num)
+values
+('pendingItemList', '代办事项列表', 'list', NULL, 'pendingItem/list', 'Y', 'N', 'Y', 'list', '2', 'page', 'enabled', '17', 'default', now(), '1'),
+('preferentialMsgList', '优惠息息列表', 'list', NULL, 'preferentialMsg/list', 'Y', 'N', 'Y', 'list', '2', 'page', 'enabled', '18', 'default', now(), '1'),
+('preferentialMsgCreation', '优惠息息创建', 'create', NULL, 'preferentialMsg/create', 'Y', 'Y', 'Y', 'edit', '2', 'page', 'enabled', '18', 'default', now(), '1'),
+('preferentialMsgEdit', '优惠息息编辑', 'edit/:id', NULL, 'preferentialMsg/edit', 'Y', 'Y', 'Y', 'edit', '2', 'page', 'enabled', '18', 'default', now(), '1'),
+('companyInfoList', '企业列表', 'list', NULL, 'companyInfo/list', 'Y', 'N', 'Y', 'list', '2', 'page', 'enabled', '19', 'default', now(), '1'),
+('companyInfoCreation', '企业创建', 'create', NULL, 'companyInfo/create', 'Y', 'Y', 'Y', 'edit', '2', 'page', 'enabled', '19', 'default', now(), '1'),
+('companyInfoEdit', '企业编辑', 'edit/:id', NULL, 'companyInfo/edit', 'Y', 'Y', 'Y', 'edit', '2', 'page', 'enabled', '19', 'default', now(), '1'),
+('feedbackList', '留言列表', 'list', NULL, 'feedback/list', 'Y', 'N', 'Y', 'list', '2', 'page', 'enabled', '20', 'default', now(), '1');
+
 insert into hhplms.ana_employee_r2_role
 (employee_sid, role_sid, created_by, created_time, version_num)
 values
@@ -59,7 +79,19 @@ values
 (1, 13, 'default', now(), '1'),
 (1, 14, 'default', now(), '1'),
 (1, 15, 'default', now(), '1'),
-(1, 16, 'default', now(), '1');
+(1, 16, 'default', now(), '1'),
+(1, 17, 'default', now(), '1'),
+(1, 18, 'default', now(), '1'),
+(1, 19, 'default', now(), '1'),
+(1, 20, 'default', now(), '1'),
+(1, 21, 'default', now(), '1'),
+(1, 22, 'default', now(), '1'),
+(1, 23, 'default', now(), '1'),
+(1, 24, 'default', now(), '1'),
+(1, 25, 'default', now(), '1'),
+(1, 26, 'default', now(), '1'),
+(1, 27, 'default', now(), '1'),
+(1, 28, 'default', now(), '1');
 
 
 
