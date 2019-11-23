bdf20fcdbf669b65813ec41eb5ff464f7bc17d27
山成 <shanc@133.cn>
2019-11-23 21:28:05 +0800

    修复验证码的bug

--- src/main/java/com/haihua/hhplms/sys/service/SmsServiceImpl.java
+++ src/main/java/com/haihua/hhplms/sys/service/SmsServiceImpl.java
@@ -3,6 +3,7 @@ package com.haihua.hhplms.sys.service;
 import com.github.qcloudsms.SmsSingleSenderResult;
 import com.haihua.hhplms.client.sms.TencentSmsClient;
 import com.haihua.hhplms.common.exception.ServiceException;
+import com.haihua.hhplms.common.utils.DateUtil;
 import com.haihua.hhplms.common.utils.JsonUtil;
 import com.haihua.hhplms.sys.model.DynamicCodeHolder;
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
@@ -46,37 +48,47 @@ public class SmsServiceImpl implements SmsService {
         if (!Objects.equals(dynamicCodeHolder.getDynamicCode(), dynamicCode)) {
             throw new ServiceException("验证码错误");
         }
+        final long currentTime = System.currentTimeMillis();
         final long expireTime = dynamicCodeHolder.getExpireTime();
-        if (System.currentTimeMillis() > expireTime) {
+        if (log.isInfoEnabled()) {
+            log.info(String.format("校验验证码 ==> 验证验证码是否过期 currentTime: %s, expireTime: %s",
+                    DateUtil.format(new Date(currentTime), "yyyy-MM-dd HH:mm:ss"),
+                    DateUtil.format(new Date(expireTime), "yyyy-MM-dd HH:mm:ss")));
+        }
+        if (currentTime > expireTime) {
             throw new ServiceException("验证码已过期");
         }
         dynamicCodeCache.remove(mobile);
     }
 
+    private void doSendDynamicCode(final String mobile) {
+        final String dynamicCode = generateDynamicCode();
+        final SmsSingleSenderResult result = tencentSmsClient.sendDynamicCode(mobile, dynamicCode);
+        if (log.isInfoEnabled()) {
+            log.info(String.format("发送验证码: %s到手机: %s, 发送结果: %s", dynamicCode, mobile, JsonUtil.toJson(result)));
+        }
+        final long currentTime = System.currentTimeMillis();
+        final DynamicCodeHolder dynamicCodeHolder = new DynamicCodeHolder(dynamicCode, currentTime + TWO_MIN_IN_MINI_SECOND);
+        dynamicCodeCache.put(mobile, dynamicCodeHolder);
+    }
+
     @Override
     public void sendDynamicCode(final String mobile) {
-        final DynamicCodeHolder dynamicCodeHolder = dynamicCodeCache.get(mobile);
-        if (Objects.isNull(dynamicCodeHolder)) {
-            final String dynamicCode = generateDynamicCode();
-            final SmsSingleSenderResult result = tencentSmsClient.sendDynamicCode(mobile, dynamicCode);
-            if (log.isInfoEnabled()) {
-                log.info(String.format("发送验证码: %s到手机: %s, 发送结果: %s", dynamicCode, mobile, JsonUtil.toJson(result)));
-            }
-
-            dynamicCodeCache.put(mobile, new DynamicCodeHolder(dynamicCode, System.currentTimeMillis() + TWO_MIN_IN_MINI_SECOND));
-            tencentSmsClient.sendDynamicCode(mobile, dynamicCode);
+        if (!dynamicCodeCache.containsKey(mobile)) {
+            doSendDynamicCode(mobile);
         } else {
+            final DynamicCodeHolder dynamicCodeHolder = dynamicCodeCache.get(mobile);
             final long expireTime = dynamicCodeHolder.getExpireTime();
-            if (System.currentTimeMillis() > expireTime) {
-                final String dynamicCode = generateDynamicCode();
-                final SmsSingleSenderResult result = tencentSmsClient.sendDynamicCode(mobile, dynamicCode);
-                dynamicCodeCache.put(mobile, new DynamicCodeHolder(generateDynamicCode(), System.currentTimeMillis() + TWO_MIN_IN_MINI_SECOND));
-                if (log.isInfoEnabled()) {
-                    log.info(String.format("发送验证码: %s到手机: %s, 发送结果: %s", dynamicCode, mobile, JsonUtil.toJson(result)));
-                }
-            } else {
+            final long currentTime = System.currentTimeMillis();
+            if (log.isInfoEnabled()) {
+                log.info(String.format("发送验证码 ==> 验证验证码是否过期 currentTime: %s, expireTime: %s",
+                        DateUtil.format(new Date(currentTime), "yyyy-MM-dd HH:mm:ss"),
+                        DateUtil.format(new Date(expireTime), "yyyy-MM-dd HH:mm:ss")));
+            }
+            if (currentTime <= expireTime) {
                 throw new ServiceException("每2分钟可以获取一次验证码, 请稍后再获取");
             }
+            doSendDynamicCode(mobile);
         }
     }
 }
